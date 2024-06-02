bef11da86807daea66201a27b5a13c81f4282495
lomserg <lomserg@gmail.com>
2024-06-02 17:29:37 +0300

    feat: add solution to first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,32 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// console.log("Let's rock");
+// console.log(translations);
+//console.log(encoded, translations);
 
+function translation(val) {
+  for (const property in translations) {
+    console.log(`${val}: ${translations[val]}`);
+  }
+}
 
-
-// console.log(decoded)
+function decoded(arr) {
+  for (let item of arr) {
+    for (let [key, value] of Object.entries(item)) {
+      if (
+        key.includes("Id") &&
+        key !== "groupId" &&
+        key !== "service" &&
+        key !== "formatSize" &&
+        key !== "ca"
+      ) {
+        // console.log(translations[value]);
+        console.log(value);
+        item[key] = translations[value];
+      }
+    }
+  }
+  return arr;
+}
+const enc = decoded(encoded);
+console.log(enc);
